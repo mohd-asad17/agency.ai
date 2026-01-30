63e0f448986a978aebd12cb7da47695fe0d9699c
mohd-asad17 <asadn01702@gmail.com>
2026-01-30 21:58:30 +0530

    footer complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Toaster } from "react-hot-toast";
+
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero";
 import CompanyLogo from "./components/Company";
@@ -6,11 +8,13 @@ import Services from "./components/Services";
 import OurWork from "./components/OurWork";
 import Teams from "./components/Teams";
 import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
   return (
     <div className="dark:bg-black relative">
+      <Toaster />
   <Navbar theme={theme} setTheme={setTheme} />
   <Hero />
   <CompanyLogo />
@@ -18,6 +22,7 @@ function App() {
   <OurWork />
   <Teams />
   <ContactUs />
+  <Footer theme={theme} />
     </div>
   )
 }
